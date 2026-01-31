a3e1ea01452bfc3c534c1e5b50b3892f95acac6d
zack1212-22 <ffgutg5567ggugh@gmail.com>
2026-02-01 01:36:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// รับคำสั่งแจ้งเตือน
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return clients.openWindow('/');
+    })
+  );
+});
